12d4de997724bb7a25e89d7a0a461f84f661d3f6
Samuel Baptista <samuel.baptista@ecodev.ch>
2016-02-20 00:18:59 +0100

    Add gulp watch, live and bs

--- gulpfile.js
+++ gulpfile.js
@@ -4,11 +4,13 @@ var uglify = require('gulp-uglify');
 var cssnano = require('gulp-cssnano');
 var concat = require('gulp-concat');
 var flatten = require('gulp-flatten');
-
+var watch = require('gulp-watch');
+var browserSync = require('browser-sync');
 
 var paths = {
     scripts: 'src/**/*.js',
-    styles: 'src/**/*.css',
+    styles: 'src/css/*.css',
+    themes: 'src/css/*/*.css',
     images: ['src/**/*.gif','src/**/*.jpg','src/**/*.jpeg','src/**/*.png'],
     dest: 'dist'
 };
@@ -21,6 +23,11 @@ gulp.task('scripts', function() {
 });
 
 gulp.task('styles', function() {
+
+    gulp.src(paths.themes)
+               .pipe(cssnano())
+               .pipe(gulp.dest(paths.dest));
+
     return gulp.src(paths.styles)
                .pipe(cssnano())
                .pipe(concat('natural-gallery.min.css'))
@@ -33,4 +40,30 @@ gulp.task('images', function() {
                .pipe(gulp.dest(paths.dest));
 });
 
+gulp.task('bs', function () {
+    browserSync({
+        proxy: 'natural-gallery.lan/demo'
+    });
+});
+
+gulp.task('reload', function () {
+    browserSync.reload();
+});
+
 gulp.task('build', ['scripts', 'styles', 'images']);
+
+gulp.task('live', ['build', 'bs', 'watch']);
+
+gulp.task('watch', ['build'], function () {
+
+    // Source files
+    gulp.watch(paths.styles, ['styles', 'reload']);
+    gulp.watch(paths.themes, ['styles', 'reload']);
+    gulp.watch(paths.scripts, ['scripts', 'reload']);
+
+    // Demo files
+    gulp.watch('demo/*.html', ['reload']);
+    gulp.watch('demo/*.css', ['reload']);
+
+});
+
