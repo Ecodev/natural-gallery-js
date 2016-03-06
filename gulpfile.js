dabfbd530159171e3be51edfe0108ace41f470be
Samuel Baptista <samuel.baptista@ecodev.ch>
2016-03-06 17:16:31 +0100

    Update gulpfile to manage errors

--- gulpfile.js
+++ gulpfile.js
@@ -6,6 +6,7 @@ var concat = require('gulp-concat');
 var flatten = require('gulp-flatten');
 var watch = require('gulp-watch');
 var browserSync = require('browser-sync');
+var handleErrors = require('./gulp/handleErrors');
 
 var paths = {
     scripts: 'src/**/*.js',
@@ -15,34 +16,50 @@ var paths = {
     dest: 'dist'
 };
 
+/**
+ * Javascript
+ */
 gulp.task('scripts', function() {
     return gulp.src(paths.scripts)
-               .pipe(uglify())
+               .pipe(uglify().on('error', handleErrors))
                .pipe(concat('natural-gallery.min.js'))
                .pipe(gulp.dest(paths.dest));
 });
 
+/**
+ * Stylesheets
+ */
 gulp.task('styles', function() {
 
     gulp.src(paths.themes)
         .pipe(gulp.dest(paths.dest));
 
     return gulp.src(paths.styles)
-               .pipe(cssnano())
+               .pipe(cssnano().on('error', handleErrors))
                .pipe(concat('natural-gallery.min.css'))
                .pipe(gulp.dest(paths.dest));
 });
 
+/**
+ * Images
+ */
 gulp.task('images', function() {
     return gulp.src(paths.images)
                .pipe(flatten())
                .pipe(gulp.dest(paths.dest));
 });
 
+/**
+ * Browsersync
+ */
 gulp.task('bs', function () {
     browserSync({
         proxy: 'natural-gallery.lan/demo'
     });
+});
+
+gulp.task('refresh', function () {
+    console.info('******************** REFRESH ***************************');
 });
 
 gulp.task('reload', function () {
@@ -50,19 +67,17 @@ gulp.task('reload', function () {
 });
 
 gulp.task('build', ['scripts', 'styles', 'images']);
-
 gulp.task('live', ['build', 'bs', 'watch']);
-
 gulp.task('watch', ['build'], function () {
 
     // Source files
-    gulp.watch(paths.styles, ['styles', 'reload']);
-    gulp.watch(paths.themes, ['styles', 'reload']);
-    gulp.watch(paths.scripts, ['scripts', 'reload']);
+    gulp.watch(paths.styles, ['refresh', 'styles', 'reload']);
+    gulp.watch(paths.themes, ['refresh', 'styles', 'reload']);
+    gulp.watch(paths.scripts, ['refresh', 'scripts', 'reload']);
 
     // Demo files
-    gulp.watch('demo/*.html', ['reload']);
-    gulp.watch('demo/*.css', ['reload']);
+    gulp.watch('demo/*.html', ['refresh', 'reload']);
+    gulp.watch('demo/*.css', ['refresh', 'reload']);
 
 });
 
