d0bf51a552b04884411d6eaf5a8fbae6dacb9a62
Samuel Baptista <samuel.baptista@ecodev.ch>
2017-07-05 15:56:52 +0200

    Add TSlint

---
+++ bin/pre-commit.sh
@@ -0,0 +1,24 @@
+#!/bin/sh
+
+pass=true
+
+files=$(git diff --cached --name-only --diff-filter=ACMR | grep -E '\.ts$')
+if [ "$files" != "" ]; then
+
+    # Run TSLint validation before commit
+    for file in ${files}; do
+        ./node_modules/.bin/tslint ${file}
+        if [ $? -ne 0 ]; then
+            pass=false
+        fi
+    done
+fi
+
+if $pass; then
+    exit 0
+else
+    echo ""
+    echo "PRE-COMMIT HOOK FAILED:"
+    echo "Code style validation failed. Please fix errors and try committing again."
+    exit 1
+fi
