260e605bb2af143599a8c7efc3eeac4287c9a25e
Adrien Crivelli <adrien.crivelli@gmail.com>
2021-05-13 19:29:20 +0900

    Export only carefuly selected types, and all from same export site

--- src/index.ts
+++ src/index.ts
@@ -1,5 +1,18 @@
 import './styles/natural-gallery.scss';
 
-export * from './js/galleries/Natural';
-export * from './js/galleries/Masonry';
-export * from './js/galleries/Square';
+export {ModelAttributes} from './js/galleries/AbstractGallery';
+
+export {
+    Natural,
+    NaturalGalleryOptions,
+} from './js/galleries/Natural';
+
+export {
+    Masonry,
+    MasonryGalleryOptions,
+} from './js/galleries/Masonry';
+
+export {
+    Square,
+    SquareGalleryOptions,
+} from './js/galleries/Square';
